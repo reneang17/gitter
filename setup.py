012345884249621ecfbf023534b27b0fcc830676
rene angele <reneang17@gmail.com>
2020-03-08 03:29:32 -0400

    removed unnecessary files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name = 'giter',
-author = 'Rene Angeles',
-author_email = 'reneang17@gmail.com',
-url = 'https://github.com/reneang17/giter',
-packages = ['Giter']
-)
